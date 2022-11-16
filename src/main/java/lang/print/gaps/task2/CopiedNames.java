d627f356e327715778dd697bfd2c01b90d20aa5a
diyorjon090 <saidovdiyorbek090@gmail.com>
2022-11-16 20:54:33 +0500

    goodd

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
+        int variable =  0;
         int variableOne = 2000;
         int value = 1000;
         int variableTwo = 4000;
